bd105db2ec0bcc46902b1eac021cf61dc88ff4d5
CoffeeTonya <127587138+CoffeeTonya@users.noreply.github.com>
2024-06-03 14:56:28 +0900

    Update breakEvenPoint.py

--- breakEvenPoint.py
+++ breakEvenPoint.py
@@ -1,90 +1,77 @@
-import datetime
-from glob import glob
-from altair.vegalite.v4.schema.core import DataFormat
-import numpy as np
-import pandas as pd
 import streamlit as st
+import numpy as np
+import datetime
+import pandas as pd
+import datetime
+import time
+import math
 
-# sidebar \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+from streamlit.proto.Checkbox_pb2 import Checkbox
 
-item_code = st.sidebar.text_input('商品コード')
-item_name = st.sidebar.text_input('商品名')
-item_cost = st.sidebar.number_input('税抜原価', 0, step=1)
-item_price = st.sidebar.number_input('税込売価', 0, step=1)
-tax = st.sidebar.selectbox(
-    '税率区分',
-    ('8%', '10%'))
-if tax == '8%':
-    tax_ = 1.08
+st.sidebar.subheader('必要事項を入力')
+cost = st.sidebar.number_input('①税抜原価（円）',  min_value=0, max_value=99999999, step=1)
+amount = st.sidebar.number_input('②仕入数量（個）',  min_value=0, max_value=99999999, step=1)
+selling = st.sidebar.number_input('③税込売価（円）',  min_value=0, max_value=99999999, step=1)
+
+if st.sidebar.button('軽減税率対象'):
+    tax = 0.08
 else:
-    tax_ = 1.10
-valiable_cost = st.sidebar.number_input('変動費', 0, step=1)
-purchases = st.sidebar.number_input('仕入数', 0, step=1)
-case = st.sidebar.number_input('入数', 0, step=1)
-st.sidebar.markdown(rf'''
-<br>
-''', unsafe_allow_html=True)
-shipping = st.sidebar.checkbox('送料無料')
+    tax = 0.10
 
-if shipping == True:
-    shipping = '無料'
-    shipping_cost = 550
+st.sidebar.write("""* * *""")
+
+varriableCost1 = st.sidebar.number_input('★変動費1/個（円）',  min_value=0, max_value=99999999, step=1)
+varriableCost2 = st.sidebar.number_input('★変動費2/個（円）',  min_value=0, max_value=99999999, step=1)
+varriableCost3 = st.sidebar.number_input('★変動費3/個（円）',  min_value=0, max_value=99999999, step=1)
+
+if cost != 0 and amount != 0 and selling != 0:
+    breakEvenPoint = math.ceil((((cost*amount)*(1+tax))+(varriableCost1*amount)+(varriableCost2*amount)+(varriableCost3*amount))/(selling))
 else:
-    shipping = '別'
-    shipping_cost = 0
+    breakEvenPoint = 0
 
-# body \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+st.title('損益分岐点計算')
+st.subheader('損益分岐点は ' + str(breakEvenPoint) + ' です')
 
-# title
-st.title('損益計算ツール')
-st.markdown(rf'''
-<br>
-''', unsafe_allow_html=True)
-if item_code == '':
-    st.write('サイドバーの入力項目を入力してください')
+st.subheader('完売時の合計')
+col7, col8, col9 = st.columns(3)
+with col7:
+    costTotal = st.info('仕入金額：' + str("{:,}".format(cost * amount)) + '円')
+with col8:
+    sellingTotal = st.info('売上金額：' + str("{:,}".format(selling * amount)) + '円')
+with col9:
+    profitTotal = st.info('粗利金額：' + str("{:,}".format((round(selling - ((cost * (1 + tax)) + (selling * (varriableCost3/100)) + (varriableCost1) + (varriableCost2))) * amount))) + '円')
 
-st.markdown(rf'''
-<br>
-''', unsafe_allow_html=True)
+units = []
+costs = []
+sellings = []
+profits = []
+profitLoss = []
+for i in range(0, amount + 1):
+    units.append(i)
+    costs_ = (math.floor((cost * (1 + tax)) * amount) + (i * varriableCost1) + (i * varriableCost2) + (i * selling * varriableCost3/100))
+    costs.append(round(costs_))
+    sellings.append(round(i * selling))
+    profits.append(i * round(selling - (cost * (1 + tax))))
 
-# item
-st.write('商品情報')
+for i in range(len(costs)):
+    tmp = round(sellings[i] - costs[i])
+    profitLoss.append(tmp)
 
-st.markdown(rf'''
-    <table>
-        <tr>
-            <th>商品CD</th><th>品名</th><th>原価</th><th>売価</th><th>送料</th><th>税率</th>
-        </tr>
-        <tr>
-            <td>{item_code}</td><td>{item_name}</td><td>{"{:,}".format(item_cost)}</td><td>{"{:,}".format(item_price)}</td><td>{shipping}</td><td>{tax}</td>
-        </tr>
-    </table>
-    ''', unsafe_allow_html=True)
+dfChart = {
+    '仕入金額': costs,
+    '売上金額': sellings,
+}
+dfTable = {
+    '販売数量': units,
+    '仕入金額': costs,
+    '売上金額': sellings,
+    '粗利金額': profits,
+    '損益金額': profitLoss,
+}
+df = pd.DataFrame(dfChart)
+df_ = pd.DataFrame(dfTable)
 
-st.markdown(rf'''
-<br>
-''', unsafe_allow_html=True)
-
-# pl_
-st.write('損益分岐表')
-
-index1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-column1 = []
-column2 = []
-column3 = []
-column4 = []
-
-for row in index1:
-    number_of_sales = row * case
-    number_of_cost = round((item_cost * purchases) + (valiable_cost * number_of_sales) + (shipping_cost * number_of_sales))
-    amount_of_sales = round(number_of_sales * (item_price / tax_))
-    plofit = round(amount_of_sales - number_of_cost)
-    column1.append("{:,}".format(number_of_sales))
-    column2.append("{:,}".format(number_of_cost))
-    column3.append("{:,}".format(amount_of_sales))
-    column4.append("{:,}".format(plofit))
-
-dict1={'売上数': column1, '仕入金額': column2, '売上金額': column3, '損益': column4}
-
-st.table(pd.DataFrame(data=dict1))
+st.subheader('推移グラフ')
+st.line_chart(df)
+st.subheader('推移一覧表')
+st.table(df_.set_index('販売数量'))
