9b6e6234bd3098fd9d0c9967404844567785b0af
CoffeeTonya <127587138+CoffeeTonya@users.noreply.github.com>
2024-06-03 12:47:38 +0900

    Update breakEvenPoint.py

--- breakEvenPoint.py
+++ breakEvenPoint.py
@@ -54,7 +54,7 @@ with col7:
 with col8:
     sellingTotal = st.info('売上金額：' + str("{:,}".format(selling * amount)) + '円')
 with col9:
-    profitTotal = st.info('粗利金額：' + str("{:,}".format((round(selling - ((cost * (1 + tax)) + (selling * (varriableCost3/100)) + (varriableCost1) + (varriableCost2) + (ships + commission))) * amount))) + '円')
+    profitTotal = st.info('粗利金額：' + str("{:,}".format((round(selling - ((cost * (1 + tax)) + (selling * (varriableCost3/100)) + (varriableCost1) + (varriableCost2))) * amount))) + '円')
 
 units = []
 costs = []
